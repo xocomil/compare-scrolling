e25d45727294938a450706579c84bd9d50e61ff2
xocomil <xocomil@gmail.com>
2023-01-22 10:17:45 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'compare-scrolling-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'compare-scrolling';
+}
